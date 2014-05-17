dcbed1e23693fc516138d18e83f32a0ac2e65958
Josh Roppo <joshroppo@gmail.com>
2014-05-17 12:07:00 -0700

    Created Unit Tests for palindromes but failed
    I'm not familiar enough with Go's syntax for structs to know exactly
    what I'm doing wrong in the data definitions.

---
+++ palindromes/palindromes_test.go
@@ -0,0 +1,24 @@
+package palindromes
+
+import "testing"
+
+func TestPalindromes(t *testing.T) {
+    /*
+    var test_data = []struct {
+        chk string, want bool
+    }{
+        {"racecar", true},
+        {"test", false},
+        {"yxxy", true},
+        {"101", true},
+        {"at", false},
+    }
+
+    for _, c := range test_data {
+        got := CheckPalindrome(c.chk)
+        if got != c.want {
+            t.Errorf("Palindrome[%q == %q], should be: %q", c.chk, got, c.want)
+        }
+    }
+    */
+}
