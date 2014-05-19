e979a2a254bdf5eca4f95c27a2031d5fc80cc5aa
Josh Roppo <joshroppo@gmail.com>
2014-05-19 16:33:09 -0700

    Figured out Array of structs syntax for tests
    An array of structs with strings and their palindrome test expectations
    now iterated over and tested against Palindrome check.

--- palindromes/palindromes_test.go
+++ palindromes/palindromes_test.go
@@ -1,34 +1,27 @@
 package palindromes
 
-import "testing"
+import (
+    "testing"
+    "fmt"
+)
 
+type TData struct { 
+    chk string
+    want bool 
+}
 func TestPalindromes(t *testing.T) {
-    /*j
-    var test_data = []struct {
-        chk string, want bool
-    }
-    test_data[0] = {"racecar", true}
+    var test_data = []TData {
+        {"racecar", true}, 
         {"test", false},
         {"yxxy", true},
         {"101", true},
         {"at", false},
-    */
+    }
 
-    /*
     for _, c := range test_data {
         got := CheckPalindrome(c.chk)
         if got != c.want {
             t.Errorf("Palindrome[%q == %q], should be: %q", c.chk, got, c.want)
         }
-    }
-    */
-    type X struct { 
-        chk string
-        want bool 
-    }
-    test := X{"yxxy", true}
-    got := CheckPalindrome(test.chk)
-    if got != test.want {
-        t.Errorf("Palindrome[%q == %q], should be: %q", test.chk, got, test.want)
     }
 }
