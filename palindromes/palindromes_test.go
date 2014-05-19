32634df8568b347f4fbb480427b450c5033faae4
Josh Roppo <joshroppo@gmail.com>
2014-05-19 16:08:15 -0700

    Finally sorted out testing syntax
    Able to test Palindrome code using single struct for data container.
    Need to sort out rest of tests and looping stuctures.

--- palindromes/palindromes_test.go
+++ palindromes/palindromes_test.go
@@ -3,17 +3,18 @@ package palindromes
 import "testing"
 
 func TestPalindromes(t *testing.T) {
-    /*
+    /*j
     var test_data = []struct {
         chk string, want bool
-    }{
-        {"racecar", true},
+    }
+    test_data[0] = {"racecar", true}
         {"test", false},
         {"yxxy", true},
         {"101", true},
         {"at", false},
-    }
+    */
 
+    /*
     for _, c := range test_data {
         got := CheckPalindrome(c.chk)
         if got != c.want {
@@ -21,4 +22,13 @@ func TestPalindromes(t *testing.T) {
         }
     }
     */
+    type X struct { 
+        chk string
+        want bool 
+    }
+    test := X{"yxxy", true}
+    got := CheckPalindrome(test.chk)
+    if got != test.want {
+        t.Errorf("Palindrome[%q == %q], should be: %q", test.chk, got, test.want)
+    }
 }
