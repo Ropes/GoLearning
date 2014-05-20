446a37a377f32f4206587aa2485e7a84f219214d
Josh Roppo <joshroppo@gmail.com>
2014-05-20 02:01:31 -0700

    Adding benchmarks to tests

--- palindromes/palindromes_test.go
+++ palindromes/palindromes_test.go
@@ -8,26 +8,40 @@ type TData struct {
     chk string
     want bool 
 }
-func TestPalindromes(t *testing.T) {
-    var test_data = []TData {
-        {"racecar", true}, 
-        {"test", false},
-        {"yxxy", true},
-        {"101", true},
-        {"at", false},
-    }
-
+var test_data = []TData {
+    {"racecar", true}, 
+    {"test", false},
+    {"yxxy", true},
+    {"101", true},
+    {"at", false},
+}
+func PalindromesIter(t *testing.T) {
     for _, c := range test_data {
         got := CheckPalindrome(c.chk)
         if got != c.want {
             t.Errorf("Palindrome[%q == %q], should be: %q", c.chk, got, c.want)
         }
     }
+}
 
-    for _, c := range test_data {
-        got := RecPalindrome(c.chk)
-        if got != c.want {
-            t.Errorf("Recursive Palindrome[%q == %q], should be: %q", c.chk, got, c.want)
+func BenchmarkPalindromesIter(t *testing.B) {
+    for i := 0; i < t.N; i++{
+        for _, c := range test_data {
+            got := CheckPalindrome(c.chk)
+            if got != c.want {
+                t.Errorf("Palindrome[%q == %q], should be: %q", c.chk, got, c.want)
+            }
+        }
+    }
+}
+
+func BenchmarkPalindromesRec(t *testing.B) {
+    for i := 0; i < t.N; i++{
+        for _, c := range test_data {
+            got := RecPalindrome(c.chk)
+            if got != c.want {
+                t.Errorf("Recursive Palindrome[%q == %q], should be: %q", c.chk, got, c.want)
+            }
         }
     }
 }
