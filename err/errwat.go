79678a7720b6be4bdb4be0de499ed9c0464e4c9b
Josh Roppo <joshroppo@gmail.com>
2014-05-27 13:19:19 -0700

    Finishing customized error message

--- err/errwat.go
+++ err/errwat.go
@@ -1,19 +1,19 @@
 package err
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 type WorseError struct {
-    When time.Time
-    Wat string
+	When time.Time
+	Wat  string
 }
 
-func (e *WorseError) Error() string{
-    return fmt.Sprintf("Whale error: '%s' @ %v", e.Wat, e.When)
+func (e *WorseError) Error() string {
+	return fmt.Sprintf("Whale error: '%s' @ %v", e.Wat, e.When)
 }
 
 func failWhale() error {
-    return &WorseError{ time.Now(), "bad error was bad" }
+	return &WorseError{time.Now(), "bad error was bad"}
 }
