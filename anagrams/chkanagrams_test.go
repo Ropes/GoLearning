c6f7004f90153295acaa67c0c6d3c83950b653c3
Josh Roppo <joshroppo@gmail.com>
2014-06-23 01:44:08 -0700

    Tests for sorting words into the anagram map

--- anagrams/chkanagrams_test.go
+++ anagrams/chkanagrams_test.go
@@ -13,4 +13,29 @@ func TestLengthChk(t *testing.T) {
 	if result == true {
 		t.Errorf("Strings of different length but true returned")
 	}
+
+	result = LenCheck(x, z)
+	if result == false {
+		t.Errorf("Strings of same length but false returned..")
+	}
+}
+
+func TestRuneSliceSort(t *testing.T) {
+	x := "hihi"
+	y := "hhii"
+
+	result := SortWord(x)
+	if result != y {
+		t.Errorf("Word wasn't sorted properly: %s", result)
+	}
+}
+
+func TestRuneSliceSort2(t *testing.T) {
+	x := "watcatw"
+	y := "aacttww"
+	result := SortWord(x)
+
+	if result != y {
+		t.Errorf("Word wasn't sorted properly: %s", result)
+	}
 }
