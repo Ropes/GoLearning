a330a73f75647f0e7f907ecfc3764b05248190cf
Josh Roppo <joshroppo@gmail.com>
2014-07-30 18:00:05 -0700

    Hello go world

--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,12 @@
 package main
 
 import "fmt"
+
 //import "github.com/ropes/palindromes"
-import "github.com/ropes/gotour"
+import "github.com/ropes/learngo/gotour"
 import "code.google.com/p/go-tour/pic"
 
-func main(){
-    fmt.Println("Hellow!!! I like Go's install so far!  Very straitforward!")
-    pic.Show(gotour.Pic)
+func main() {
+	fmt.Println("Hellow!!! I like Go's install so far!  Very straitforward!")
+	pic.Show(gotour.Pic)
 }
-
